d854bf12f77a116cdd470b4680fc18779fa6927a
SaiBhanuBandi14114141 <105918956+SaiBhanuBandi14114141@users.noreply.github.com>
2022-05-31 20:23:59 +0530

    Add files via upload

--- data.js
+++ data.js
@@ -1,12 +1,13 @@
 let submit=document.getElementById("submit");
-submit.addEventListener("click",function(e)
+submit.addEventListener("click",function()
 {
     let name=document.getElementById("name").value;
     let date=document.getElementById("date").value;
     let time=document.getElementById("time").value;
     let peoplecount=document.getElementById("peoplecount").value;
+    let email=document.getElementById("email").value;
 
-    if(name=='' || date=='' || time=='' || peoplecount<1) 
+    if(name=='' || date=='' || time=='' || peoplecount<1 || email=='') 
     {
         return alert("please add all values.....")
     }
@@ -24,14 +25,28 @@ submit.addEventListener("click",function(e)
         let myObj = 
         {
             name: name,
+            email:email,
             date: date,
             time:time,
             peoplecount:peoplecount
         }
+        
+        if(dataObj.length>0)
+        {
+            for(let i=0;i<dataObj.length;i++)
+            {
+                if(dataObj[i]['email']==email || dataObj[i]['date']==date || dataObj[i]['time']==time)
+                {
+                    return alert('Dear Customer you have already reserved the table with the email : '+email);
+                }
+            }
+        }
+
         dataObj.push(myObj);
         localStorage.setItem("data", JSON.stringify(dataObj));
+        return alert(name+' Reserved Successfully');
     }
-    alert(name+" Reserved Successfully!!!!!!!")
+    
 })
 
 
